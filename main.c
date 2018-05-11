09d8be254d45eb3fbdf06454e3206814166e8368
AlejandroGTA <36650624+AlejandroGTA@users.noreply.github.com>
2018-05-10 19:13:30 -0500

    Cambio de código
    Re-hice el código porque no eran necesarias los delays (sólo las interrupciones); hay unos pequeños errores que se tienen que modificar, las interrupciones son muy lentas.

--- main.c
+++ main.c
@@ -3,98 +3,61 @@
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
+#use fast_io(b)   //Se emplea la directiva de Fast_io
+#use fast_io(c)   //Se emplea la directiva de Fast_io
+#use fast_io(d)   //Se emplea la directiva de Fast_io
 
-//Se emplea la directiva de Fast_io
-#use fast_io(b)
-#use fast_io(c)
-#use fast_io(d)
-
-//Se declara un arreglo que alamcene los numeros que se mostraran en el display.
-int array[]={
-    0x00,//num0
-    0x01,//num1
-    0x02,//num2
-    0x03,//num3
-    0x04,//num4
-    0x05,//num5
-    0x06,//num6
-    0x07,//num7
-    0x08,//num8
-    0x09 //num9
-};
-
-//Declaramos las variables que se utilizaran,
-int numeroUno = 0;//Puerto de salida
-int desbordamiento = 0;//Puerto a nivel bajo en todos los pines
-
-//Se crea la funcion para el primer display de 500ms
-void funcionDisplay500ms (void){
-    set_tris_b(0x00);//Puerto de salida
-    output_b(0x00);//Puerto a nivel bajo en todos los pines
-    
-    //Valor de prescaler de 2
-    setup_timer_0(RTCC_INTERNAL|RTCC_DIV_2|RTCC_8_BIT);
-    
-    //Valor de timer0 aplicando formula es 6
-    set_timer0(6);
-}
-
-void funcionDisplay200ms (void){
-    set_tris_c(0x00);//Puerto de salida
-    output_c(0x00);//Puerto a nivel bajo en todos los pines
-    
-    //Valor de prescaler de 2
-    setup_timer_0(RTCC_INTERNAL|RTCC_DIV_2|RTCC_8_BIT);
-    
-    //Valor de timer0 aplicando formula es 156
-    set_timer0(156);
-}
-
-void funcionDisplay1s (void){
-    set_tris_d(0x00);
-    output_d(0x00);
-    
-    //Valor de prescaler de 1
-    setup_timer_0(RTCC_INTERNAL|RTCC_DIV_1|RTCC_8_BIT);
-    
-    //Valor de timer0 aplicando formula es 256
-    set_timer0(256);
-}
-
-//Se activa la funcion del timer0
-#int_timer0
-
-void isr_interrupcionTimer0 (void){ //Se crea la funcion
-    
-    //Se va incrementando la variable desbordamiento        
-    desbordamiento++;
-    if(desbordamiento == 9){//Cuando desbordamiento llegue a 9
-        desbordamiento = 0;//Se retorna a 0
-    }
-}
-
-//enable_interrupts(int_timer0);//Se activa la funcion interrupcion
-//enable_interrupts(global);//Se activa la funcion que desactiva la funcion
-
-void main (void){
-    
-   setup_oscillator(OSC_16MHZ);
-   setup_adc_ports(NO_ANALOGS);
-
-   while(1){
-       
-       output_b(array[numeroUno]);
-       delay_ms(500);
-       
-      
-       output_c(array[numeroUno]);
-       delay_ms(200);
-       
-       
-       output_d(array[numeroUno]);
-       delay_ms(1000);
+unsigned int contadorDoscientos = 0, contadorQuinientos = 0, contadorSegundo = 0; //Declaramos nuestros contadores
+int contador1 = 0, contador2 = 0, contador3 = 0;  //Declaramos tres contadores distintos para llevar control de nuestros delays
+int numeros[10]=  //Declaramos nuestro vector con los valores de nuestros numeros en decimal
+{
+   254, 48, 109, 121, 51, 91, 95, 112, 127, 123 
+}; //Declaramos nuestros numeros en decimal a partir de su valor en binario representado en el BCD 7 segmentos
+#int_timer0 //Declaramos que nuestra siguiente instrucción es un Timer_0
+void contadorTimer0(void) //Creamos una funcion para nuestro Timer_0
+{
+   contadorSegundo++; //Si hay una interrupcion, contador segundo se aumenta en 1
+   contadorDoscientos++; //Si hay una interrupcion, contador de doscientos se aumenta en 1
+   contadorQuinientos++; //Si hay una interrupcion, contador de quinientos se aumenta en 1
+   if(contadorDoscientos == 2) //Si nuestro contador doscientos es igual a 2
+   {
+      contador1++; //Nuestro contador 1 se aumenta en 1
+   }
+   if(contadorQuinientos == 5) //Si nuestro contador quinientos es igual a 5
+   {
+      contador2++; //Nuestro contador 2 se aumenta en 1
+   }
+   if(contadorSegundo == 10) //Si nuestro contador doscientos es igual a 10
+   {
+      contador3++; //Nuestro contador 3 se aumenta en 1
+   }
+   if(contador1 >= 10) //Si nuestro contador1 es mayor o igual a 10
+   {
+      contador1=0; //Nuestro contador 1 toma se reinicia y toma el valor de 0
+   }
+   if(contador2 >= 10) //Si nuestro contador2 es mayor o igual a 10
+   {
+      contador2=0; //Nuestro contador 1 toma se reinicia y toma el valor de 0
+   }
+   if(contador3 >= 10) //Si nuestro contador3 es mayor o igual a 10
+   {
+      contador3=0; //Nuestro contador 1 toma se reinicia y toma el valor de 0
    }
 }
-  void funcionDisplay500ms();
-  void funcionDisplay200ms();
-  void funcionDisplay1s();
+void main(void) //Programa principal
+{
+   setup_oscillator(OSC_16MHZ); //Configuramos nuestro oscillador a 16MHZ
+   set_tris_b(0x00); //Declaramos nuestros puertos en modo salida
+   set_tris_c(0x00); //Declaramos nuestros puertos en modo salida 
+   set_tris_d(0x00); //Declaramos nuestros puertos en modo salida
+   setup_timer_0(RTCC_INTERNAL|RTCC_DIV_256|RTCC_8_BIT); //Habilitamos el reloj interno, el prescaler 2 y los 8 bits a trabajar
+   set_timer0(8); //Aplicamos calculos y empleamos el prescaler de 8
+   enable_interrupts(GLOBAL); //Habilitamos interrupciones globales
+   enable_interrupts(INT_TIMER0); //Habilitar la interrupcion int_timer0
+   while(1) //Abrimos un ciclo infinito
+   {
+      output_b(numeros[contador1]); //Sacamos el valor de nuestro contador 1 a traves de enuestro puerto B
+      output_c(numeros[contador2]); //Sacamos el valor de nuestro contador 2 a traves de enuestro puerto C
+      output_d(numeros[contador3]); //Sacamos el valor de nuestro contador 3 a traves de enuestro puerto D
+   }
+}
